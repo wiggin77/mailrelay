72a3be73157003b3c145b402a9a9de6d0c7baf4b
Quentin Lemoigne <quentinlemoigne14@gmail.com>
2024-07-07 22:40:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,3 +15,7 @@ env GOOS=darwin GOARCH=amd64 go build -o ./build/osx_amd64/mailrelay-osx-amd64
 # build OpenBSD
 echo building OpenBSD...
 env GOOS=openbsd GOARCH=amd64 go build -o ./build/openbsd_amd64/mailrelay-openbsd-amd64
+
+# build Linux ARM64 (Raspberry PI)
+echo building Linux ARM64 (Raspberry PI)...
+env GOOS=linux GOARCH=arm go build -o ./build/linux_amd64/mailrelay-linux-arm64
